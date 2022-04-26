cb6c1ccd85f507f4213141fd1ad8bc6fec75aa7e
Lizzie <lizziewrab@me.com>
2022-04-26 15:09:27 -0500

    commit tree 4f0d71d18d3b1812679a08f40d9a1e0550cab4a2

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,20 @@
 # Write your code here.
+def badge_maker (name)
+    "Hello, my name is #{name}." 
+end
+
+def batch_badge_creator (names)
+    names.map {|name| "Hello, my name is #{name}."}
+end
+
+def assign_rooms (names)
+    names.map.each_with_index {|name, i| "Hello, #{name}! You'll be assigned to room #{i+1}!"}
+end
+
+def printer(names)
+    intros = batch_badge_creator(names)
+    rooms = assign_rooms(names)
+    intros.each {|intro| puts intro}
+    rooms.each {|room| puts room}
+
+end
